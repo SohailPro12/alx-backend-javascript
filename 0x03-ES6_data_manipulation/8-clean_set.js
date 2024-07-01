75119b7af57b90ba8668ee89f7f03e35295f7ee1
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-01 14:07:37 +0200

    task 7 is done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,8 +3,8 @@ export default function cleanSet(set, startString) {
   for (let item of set) {
     if (item.startsWith(startString)) {
       item = item.slice(startString.length);
+      result.push(item);
     }
-    result.push(item);
   }
   return result.join('-');
 }
