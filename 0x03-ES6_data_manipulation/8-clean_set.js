feb04c83431ec63a529932f00cb09a6ab197349a
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-01 14:09:17 +0200

    task 8 is done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return;
+  }
   let result = [];
   for (let item of set) {
     if (item.startsWith(startString)) {
