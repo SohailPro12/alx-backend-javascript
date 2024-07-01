67864693699a85841d440249c122cf603d72e82e
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-01 12:50:15 +0200

    task 4 is done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,3 @@
 export default function updateStudentGradeByCity(Students, city, newGrades) {
- return Students.filter((Student) => newGrades.id === Student.id)
-    .map((Student) => {
-        if (Student.location === city && Student.id === newGrades.id) {
-            Student.grade = newGrades.grade;
-        }
-    });
+ return Students.filter((Student) => newGrades.id === Student.id);
 }
