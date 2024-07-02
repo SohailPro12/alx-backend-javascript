dc2088abe07055a63dd0341c3ec3a30e08dc59a9
Sohail Charef <charefsohayl@gmail.com>
2024-07-02 09:03:36 +0000

    fix errors

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,8 +1,3 @@
-import updateUniqueItems from "./10-update_uniq_items.js";
-import groceriesList from "./9-groceries_list.js";
+import getListStudents from "./0-get_list_students.js";
 
-const map = groceriesList();
-console.log(map);
-
-updateUniqueItems(map)
-console.log(map);
+console.log(getListStudents());
