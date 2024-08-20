2f5e7f2c91945d4c923ce7b851aac9caa3c3fb56
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-20 12:36:40 +0200

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    const lines = data.split('\n');
+    let students = 0;
+    lines.forEach((line) => {
+      if (line) {
+        students += 1;
+      }
+    });
+    console.log(`Number of students: ${students - 1}`);
+
+    // Extract the headers (first line)
+    const headers = lines[0].trim().split(',');
+
+    // Convert each remaining line into an object
+    const objects = lines.slice(1).map((line) => {
+      const values = line.trim().split(',');
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header] = values[index];
+      });
+      return obj;
+    });
+
+    const fieldCounts = {};
+
+    // Iterate over the array of objects
+    const listOfStudentsbyfield = {};
+    objects.forEach((student) => {
+      const { field } = student;
+
+      // If the field is already in the fieldCounts object, increment the count
+      if (fieldCounts[field]) {
+        fieldCounts[field] += 1;
+        listOfStudentsbyfield[field].push(student.firstname);
+      } else {
+        // Otherwise, initialize the count for that field
+        fieldCounts[field] = 1;
+        listOfStudentsbyfield[field] = [student.firstname];
+      }
+    });
+    for (const field in fieldCounts) {
+      if (Object.hasOwn(fieldCounts, field)) {
+        console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${listOfStudentsbyfield[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+module.exports = countStudents;
