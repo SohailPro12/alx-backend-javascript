2b28446041a9edc8e8641427900807b07fbda1da
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-22 17:21:30 +0200

    task 9

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,16 +2,27 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-  it('should return status code 200', (done) => {
-    request.get('http://localhost:7865/', (error, response, body) => {
+  it('should return status code 200 and correct message', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return status code 200 and correct message for valid id', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
       done();
     });
   });
 
-  it('should return the correct message', (done) => {
-    request.get('http://localhost:7865/', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
+  it('should return status code 404 for invalid id', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
