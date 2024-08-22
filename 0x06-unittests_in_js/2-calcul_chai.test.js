8ceb6b41effd2f964d7a567d19d03d9013e54139
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-22 16:00:43 +0200

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,9 +1,8 @@
-const chai = import('chai');
-const { expect } = chai;
+const expect  = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function() {
-  it('should return 6 when inputs are 1.4 and 4.5 and the type is SUM', function() {
+  it('should return 4 when inputs are 1.4 and 4.5 and the type is SUM', function() {
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 
@@ -15,7 +14,7 @@ describe('calculateNumber', function() {
     expect(calculateNumber('DIVIDE', 10, 2)).to.equal(5);
   });
 
-  it('should return "Error" when inputs are 7 and 0 and the type is DIVIDE', function() {
+  it('should return Error when inputs are 7 and 0 and the type is DIVIDE', function() {
     expect(calculateNumber('DIVIDE', 7, 0)).to.equal('Error');
   });
 });
